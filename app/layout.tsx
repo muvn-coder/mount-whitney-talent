9e2476903f417f4b254f7d8b21bacf069b5328f4
Arsene-MN <arsenemuvunyi27@gmail.com>
2025-09-03 22:36:44 +0200

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,18 @@ import { ScrollToTop } from '@/components/scroll-to-top'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Mount Whitney Talent',
-  description: 'Mount Whitney Talent',
-  generator: 'talent',
+  title: 'Mount Whitney Talent - We build your world',
+  description: 'We are a digital agency with extensive experience in Web, App, IT Solutions, Digital Marketing, Brand Communication Solutions.',
+  generator: 'Mount Whitney Talent',
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.ico' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
+  manifest: '/site.webmanifest',
 }
 
 export default function RootLayout({
